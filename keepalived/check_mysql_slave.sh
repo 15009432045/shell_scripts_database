c0c4db35f26f965cae568cb836b9ac7ce2309cdf
braior <48779728+braior@users.noreply.github.com>
2019-05-29 19:26:33 +0800

    Add files via upload

--- keepalived/check_mysql_slave.sh
+++ keepalived/check_mysql_slave.sh
@@ -1,1 +1,15 @@
-1
+#!/bin/bash
+
+export IP=`ifconfig | grep Ethernet -A1 | grep -v Link | awk -F[' ':]+ '{print $4}'`
+MYUSER=root
+MYPASS="zxs142857.*"
+MYSQL_PATH=/usr/bin/
+MYSQL_CMD="$MYSQL_PATH/mysql -u$MYUSER -p$MYPASS"
+
+SlaveStatus=($($MYSQL_CMD  -e "show slave status\G" | egrep "_Running"|awk '{print $NF}'))
+
+if [ "${SlaveStatus[0]}" = "No" ] || [ "${SlaveStatus[1]}" = "No" ]
+then
+    systemctl stop keepalived
+fi
+
