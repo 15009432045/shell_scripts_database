2dfc1d608b3b4757356adc5934c509d7c6f8e00f
braior <48779728+braior@users.noreply.github.com>
2019-06-03 17:07:05 +0800

    Update sendmail.sh

--- zabbix/sendmail.sh
+++ zabbix/sendmail.sh
@@ -1,3 +1,7 @@
+#set from=braior@163.com smtp=smtp.163.com 
+#set smtp-auth-user=braior@163.com smtp-auth-password=ZXS142857
+#set smtp-auth=login
+
 #!/bin/sh
 #export.UTF-8
 echo "$3" | sed s/'\r'//g | mailx -s "$2" $1
