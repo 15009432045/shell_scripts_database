2bb20bd1e12581f864e8611a583759679760351a
braior <48779728+braior@users.noreply.github.com>
2019-06-03 21:18:21 +0800

    Update check_mysql_slave.sh

--- keepalived/check_mysql_slave.sh
+++ keepalived/check_mysql_slave.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# auth: braior
+# email: braior@163.com
+# github: https://github.com/braior
+
 export IP=`ifconfig | grep Ethernet -A1 | grep -v Link | awk -F[' ':]+ '{print $4}'`
 MYUSER=root
-MYPASS="zxs142857.*"
+MYPASS="password"
 MYSQL_PATH=/usr/bin/
 MYSQL_CMD="$MYSQL_PATH/mysql -u$MYUSER -p$MYPASS"
 
