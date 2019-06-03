aa69cb726e4b4d6886c36c904ec8effff331ac4e
braior <48779728+braior@users.noreply.github.com>
2019-06-03 21:21:14 +0800

    Update check_nginx.sh

--- keepalived/check_nginx.sh
+++ keepalived/check_nginx.sh
@@ -1,4 +1,9 @@
-#!/bin/bash											        	
+#!/bin/bash	
+
+# Author: braior
+# Email: braior@163.com
+# Github: https://github.com/braior
+
 /usr/bin/curl -I http://localhost &>/dev/null	
 if [ $? -ne 0 ];then									    	
 	systemctl stop keepalived.service
