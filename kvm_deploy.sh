5b080bc555318066df8e8a6645fddd7a606f9d91
braior <48779728+braior@users.noreply.github.com>
2019-06-12 21:56:48 +0800

    Add files via upload

--- kvm_deploy.sh
+++ kvm_deploy.sh
@@ -37,7 +37,27 @@ function menu(){
 "
 }
 
-
+function install_kvm(){
+    rpm -qa |grep virt-manager &>/dev/null
+    if [ $? -ne 0 ];then
+        echo "Beginning install kvm..."
+        yum -y install *virt* *qemu* *kvm*
+        
+        systemctl start virt-manger || systemctl start libvirtd 
+        
+        if [ $? -eq 0 ];then
+            systemctl enable virt-manger || systemctl enable libvirtd 
+            echo "kvm was installed and running!"
+        fi
+    else
+        systemctl enable virt-manger || systemctl enable libvirtd
+        if [ $? -eq 0 ];then
+            printf "kvm is already installed and running!\n"
+        else
+            printf "Please check if the kvm application is properly installed or configured！！！"
+        fi
+    fi
+}
 function virsh_list(){
     list=(`virsh list --all  |grep "centos*" |awk '{print $2}'`)    
     list_state=(`virsh list --all |grep "centos*" |awk '{print $3$4}'`)
@@ -56,31 +76,82 @@ function virsh_list(){
     done
 }
 
+function batches_virtual_machine(){
+    
+    read -p "please enter the numbers of your want: " choose
+    #wget -O $xml_dir/centos7u3_base.xml   ftp://10.3.145.114/kvm/centos7u3_base.xml
+    if grep '^[[:digit:]]*$' <<< "$choose";then 
+        for i in `seq $choose`
+        do
+            get_mac
+            printf "starting create virtual machine..."
+            now_num=$mac_as_num
+            cp $xml_dir/centos7u3_base.xml  $xml_dir/centos7u3_$now_num.xml
+            cp $images_dir/centos7u3_base.qcow2  $images_dir/centos7u3_$now_num.qcow2
+            vm_name=centos7u3_$now_num
+            vm_uuid=`uuidgen`
+            vm_disk=centos7u3_$now_num.qcow2
+            vm_mac=$mac
+             
+            sed -i 's/VM_NAME/'${vm_name}'/'   $xml_dir/centos7u3_$now_num.xml
+            sed -i 's/VM_UUID/'${vm_uuid}'/'   $xml_dir/centos7u3_$now_num.xml
+            sed -i 's/VM_DISK/'${vm_disk}'/'   $xml_dir/centos7u3_$now_num.xml
+            sed -i 's/VM_MAC/'${vm_mac}'/'   $xml_dir/centos7u3_$now_num.xml
+            virsh define $xml_dir/centos7u3_${now_num}.xml
+       done
+    else
+        printf "ERROR!The number you entered is error.\n"
+        exit 1
+    fi    
+}
+function del_virtual_machine(){
+   read -p "please input you want delete vir_mac name: " vir_mac_del
+   if echo "${list[@]}" | grep -w "$vir_mac_del" &>/dev/null;then
+       rm -rf $xml_dir/${vir_mac_del}.xml && rm -rf  $images_dir/${vir_mac_del}.qcow2
+       virsh undefine ${vir_mac_del}
+       printf "successfully deleted！"
+   else
+       printf "ERROR!The virtual machine name you entered is incorrect.\n"
+       exit 1
+   fi
+}
+function open_virtual_machine(){
+   read -p "please input you want open virtual machine: " vir_mac_open
+   if echo "${list[@]}" | grep -w "$vir_mac_open" &>/dev/null;then
+       virsh start ${vir_mac_open}
+       wait
+       virsh list --all |grep ${vir_mac_open} |grep "running" &>/dev/null
+       if [ $? -eq 0 ];then
+           printf "The virtual machine ${vir_mac_open} is successfully started!\n"
+       else
+           printf "${vir_mea_open} Virtual machine failed to start,please try again!"
+           exit 1
+       fi
+   else
+       printf "ERROR!The virtual machine name you entered is incorrect.\n"
+   fi
+}
+function stop_virtual_machine(){
+    read -p "please input you want stop virtual machine: " vir_mac_stop
+    if echo "${list[@]}" | grep -w "$vir_mac_stop" &>/dev/null;then
+        virsh shutdown ${vir_mac_stop}
+        wait
+        if ! virsh list --all |grep ${vir_mac_stop} |grep "shut off";then
+            printf "The virtual machine ${vir_mac_stop} is successfully stop!\n"
+        else
+            printf "Stop failure,please try again!\n"
+            exit 1
+        fi
+    else
+       printf "ERROR!The virtual machine name you entered is incorrect.\n"
+    fi
+}
 while true
 do
     menu
     read -p "请输入选项：" choice
     case $choice in
     1)
-        rpm -qa |grep virt-manager &>/dev/null
-        if [ $? -ne 0 ];then
-            echo "Beginning install kvm..."
-            yum -y install *virt* *qemu* *kvm*
-            
-            systemctl start virt-manger || systemctl start libvirtd 
-            
-            if [ $? -eq 0 ];then
-                systemctl enable virt-manger || systemctl enable libvirtd 
-                echo "kvm was installed and running!"
-            fi
-        else
-            systemctl enable virt-manger || systemctl enable libvirtd
-            if [ $? -eq 0 ];then
-                printf "kvm is already installed and running!\n"
-            else
-                printf "Please check if the kvm application is properly installed or configured！！！"
-            fi
-        fi
         ;;
     2)
         virsh_list
@@ -89,102 +160,36 @@ do
         clear
         ;;
     3)
-       read -p "please enter the numbers of your want: " choose
-       #wget -O $xml_dir/centos7u3_base.xml   ftp://10.3.145.114/kvm/centos7u3_base.xml
-       
-       for i in `seq $choose`
-       do
-           get_mac
-           printf "starting create virtual machine..."
-           now_num=$mac_as_num
-           cp $xml_dir/centos7u3_base.xml  $xml_dir/centos7u3_$now_num.xml
-           cp $images_dir/centos7u3_base.qcow2  $images_dir/centos7u3_$now_num.qcow2
-           vm_name=centos7u3_$now_num
-           vm_uuid=`uuidgen`
-           vm_disk=centos7u3_$now_num.qcow2
-           vm_mac=$mac
-            
-           sed -i 's/VM_NAME/'${vm_name}'/'   $xml_dir/centos7u3_$now_num.xml
-           sed -i 's/VM_UUID/'${vm_uuid}'/'   $xml_dir/centos7u3_$now_num.xml
-           sed -i 's/VM_DISK/'${vm_disk}'/'   $xml_dir/centos7u3_$now_num.xml
-           sed -i 's/VM_MAC/'${vm_mac}'/'   $xml_dir/centos7u3_$now_num.xml
-          
-           virsh define $xml_dir/centos7u3_${now_num}.xml
-      done
-      ;;
+        batches_virtual_machine
+        ;;
     4)  
-       virsh_list
-       read -p "please input you want delete vir_eng name: " vir_eng
-       if echo "${list[@]}" | grep -w "$vir_eng" &>/dev/null;then
-           rm -rf $xml_dir/${vir_eng}.xml && rm -rf  $images_dir/${vir_eng}.qcow2
-           virsh undefine ${vir_eng}
-           printf "successfully deleted！"
-       else
-           printf "ERROR!The virtual machine name you entered is incorrect.\n"
-           exit 1
-       fi
-       ;;
+        virsh_list
+        del_virtual_machine
+        ;;
     5)
-       virsh_list
-       read -p "please input you want open vir_mea: " vir_eng_open
-       if echo "${list[@]}" | grep -w "$vir_eng_open" &>/dev/null;then
-           virsh start ${vir_eng_open}
-           wait
-           virsh list --all |grep ${vir_eng_open} |grep "running" &>/dev/null
-           if [ $? -eq 0 ];then
-               printf "The virtual machine ${vir_mea_open} is successfully started!\n"
-           else
-               printf "${vir_mea_open} Virtual machine failed to start,please try again!"
-               exit 1
-           fi
-       else
-           printf "ERROR!The virtual machine name you entered is incorrect.\n"
-       fi
-       ;;
+        virsh_list
+        open_virtual_machine
+        ;;
     6)
-      virsh_list
-      read -p "please input you want stop vir_mea: " vir_eng_stop
-      if echo "${list[@]}" | grep -w "$vir_eng_stop" &>/dev/null;then
-          virsh shutdown ${vir_eng_stop}
-          wait
-          if virsh list --all |grep ${vir_eng_stop} |grep "shut off";then
-              printf "The virtual machine ${vir_eng_stop} is successfully started!\n"
-          else
-              printf "Stop failure,please try again!\n"
-              exit 1
-          fi
-      else
-         printf "ERROR!The virtual machine name you entered is incorrect.\n"
-      fi
-      ;;
+        virsh_list
+        stop_virtual_machine
+        ;;
     7) 
-      virsh_list
-      read -p "请选择你要克隆的虚拟机" clone_name
-      vm_name=$clone_name-clone
-      virt-clone -o ${clone_name} -n ${vm_name} --auto-clone
-     # vm_uuid=$uuid_base$clone
-     # vm_disk=$vm_name.qcow2
-     # 
-     # cp $xml_dir/${clone_name}.xml  $xml_dir/$vm_name.xml
-     # sed -i 's/VM_NAME/'${vm_name}'/'   $xml_dir/$clone_name-clone.xml
-     # sed -i 's/VM_UUID/'${vm_uuid}'/'   $xml_dir/$clone_name-clone.xml
-     # sed -i 's/VM_DISK/'${vm_disk}'/'   $xml_dir/$clone_name-clone.xml
-     # sed -i 's/VM_MAC/'${vm_mac}'/'     $xml_dir/$clone_name-clone.xml
-     # cp $images_dir/${clone_name}.qcow2  $images_dir/$vm_name.qcow2
-      virsh define $xml_dir/$vm_name.xml
-      ;;
+        virsh_list
+        read -p "请选择你要克隆的虚拟机" clone_name
+        vm_name=$clone_name-clone
+        virt-clone -o ${clone_name} -n ${vm_name} --auto-clone
+        virsh define $xml_dir/$vm_name.xml
+        ;;
     q)
-      exit
-      ;;      
-        
+        exit
+        ;;        
     h)      
-      :
-    ;;
-
+        :
+        ;;
     *) 
-      echo "无效的选择，请重新输入"
-    ;;
-
+        echo "无效的选择，请重新输入"
+        ;;
     esac
 done
 
